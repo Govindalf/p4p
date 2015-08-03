afe5c0f0dc85fb79991a9cab21b0b1f237b0b8a3
Rodel Rojos <rroj173@aucklanduni.ac.nz>
2015-08-03 19:51:53 +1200

    Added new window to prompt for the project/folder to add the file in.  Name is stored nto a string.

---
+++ P4PSpeechDB/P4PSpeechDB/FolderMsgPrompt.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P4PSpeechDB
+{
+    /// <summary>
+    /// Interaction logic for FolderMsgPrompt.xaml
+    /// </summary>
+    public partial class FolderMsgPrompt : Window
+    {
+
+        public enum InputType
+        {
+            Text,
+            Password
+        }
+
+        private InputType _inputType = InputType.Text;
+
+        public FolderMsgPrompt(string question, string title, string defaultValue = "", InputType inputType = InputType.Text)
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(PromptDialog_Loaded);
+            txtCreateFolder.Text = question;
+            Title = title;
+            txtFolderName.Text = defaultValue;
+            txtFolderName.Visibility = Visibility.Visible;
+        }
+
+        public static string Prompt(string question, string title, string defaultValue = "", InputType inputType = InputType.Text)
+        {
+            FolderMsgPrompt inst = new FolderMsgPrompt(question, title, defaultValue, inputType);
+            inst.ShowDialog();
+            if (inst.DialogResult == true)
+                return inst.txtFolderName.Text;
+            //System.Console.WriteLine();
+            return null;
+        }
+
+        void PromptDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtFolderName.Focus();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}

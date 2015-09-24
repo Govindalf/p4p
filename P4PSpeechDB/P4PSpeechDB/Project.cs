6ad385082a6c6a2767d1b0ca9736b5e7ea7461a7
Rodel Rojos <rroj173@aucklanduni.ac.nz>
2015-09-24 20:00:53 +1200

    Reset to attempt

---
+++ P4PSpeechDB/P4PSpeechDB/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4PSpeechDB
+{
+    public class Project
+    {
+        public string PID { get; set; }
+        public string DateCreated { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ P4PSpeechDB/P4PSpeechDB/Speaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4PSpeechDB
+{
+
+    public class Speaker
+
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        //public string FilePath { get; set; }
+        public string PID { get; set; }
+        public string FileType { get; set; }
+        public string SpeakerName { get; set; }
+        public string Age { get; set; }
+    }
+}
